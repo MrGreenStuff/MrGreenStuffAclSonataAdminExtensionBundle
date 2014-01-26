4d01f31665c36c7dc007ab67bc77b3e51ea2b174
MrGreenStuff <juillard.yoann@gmail.com>
2014-01-26 20:33:55 +0100

    Update AclAdminExtension.php

--- Admin/AclAdminExtension.php
+++ Admin/AclAdminExtension.php
@@ -56,10 +56,13 @@ class AclAdminExtension extends AdminExtension
      * @param  string              $context
      * @throws \RuntimeException
      */
-    public function configureQuery(AdminInterface $admin, ProxyQueryInterface $query, $context = 'list')
+    public function configureQuery(AdminInterface $admin, ProxyQueryInterface $query=null,$query2=null, $context = 'list',$isTheMaster=false)
     {
+		if(!$query){
+			$query=$query2;
+		}
         // Don't filter for admins and for not ACL enabled classes and for command cli
-        if (!$admin->isAclEnabled() || !$this->securityContext->getToken() || $admin->isGranted(sprintf($admin->getSecurityHandler()->getBaseRole($admin), 'ADMIN'))) {
+        if ((!$admin->isAclEnabled() && !method_exists($admin,'getMasterAclClass')) || !$this->securityContext->getToken() || $admin->isGranted(sprintf($admin->getSecurityHandler()->getBaseRole($admin), 'ADMIN'))) {
             return;
         }
 
@@ -80,10 +83,9 @@ class AclAdminExtension extends AdminExtension
         $classType = $admin->getClass();
         $classStmt = $this->databaseConnection->prepare('SELECT id FROM acl_classes WHERE class_type = :classType');
         $classStmt->bindValue('classType', $classType);
-        $classStmt->execute();
-
+        $classStmt->execute();		
         $classId = $classStmt->fetchColumn();
-        if ($identityId && $classId) {
+		if ($identityId && ($classId || method_exists($admin,'getMasterAclClass'))) {
             $entriesStmt = $this->databaseConnection->prepare('SELECT object_identifier FROM acl_entries AS ae JOIN acl_object_identities AS aoi ON ae.object_identity_id = aoi.id WHERE ae.class_id = :classId AND ae.security_identity_id = :identityId AND (:view = ae.mask & :view OR :operator = ae.mask & :operator OR :master = ae.mask & :master OR :owner = ae.mask & :owner)');
             $entriesStmt->bindValue('classId', $classId);
             $entriesStmt->bindValue('identityId', $identityId);
@@ -120,27 +122,32 @@ class AclAdminExtension extends AdminExtension
 				}
 				$parents=$admin->getMasterAclPath();
 				//HERE UPDATE THE QUERY
-				foreach($parents as $key=>$parent){
-					//FIRST shorcut is 'o'
-					if($key==0){
-						$query->leftJoin('o.'.$parent[0],$parent[1]);
-					}else{
-					//Shortcut is precedent shortcut
-						$query->leftJoin($parents[$key-1][1].'.'.$parent[0],$parent[1]);
-					}
-					//HERE WE ARE AFTER THE LEFT JOIN ON MASTER ACL CLASS WE PASS idsMaster array param
-					if(($key+1)==count($parents)){
-						//HERE FOR OBJECT CREATED BY CURRENT USER AND WITH STRICT MODE IS OF
-						if(count($ids) && method_exists($admin,'getMasterAclStrict') && !$admin->getMasterAclStrict()){
-							//OR EXPRESSION WITH PARENTHESIS
-							$orCondition = $query->expr()->orx();
-							$orCondition->add($query->expr()->in('o.id', ':ids'));
-							$orCondition->add($query->expr()->in($parent[1].'.id',':idsMaster'));
-							$query->andWhere($orCondition)->setParameter('ids', $ids)->setParameter('idsMaster', $idsMaster);
+				if(!$isTheMaster){
+					//HERE UPDATE THE QUERY
+					foreach($parents as $key=>$parent){
+						//FIRST shorcut is 'o'
+						if($key==0){
+							$query->leftJoin('o.'.$parent[0],$parent[1]);
 						}else{
-							$query->andWhere($parent[1].'.id IN (:idsMaster'.$key.')')->setParameter('idsMaster'.$key, $idsMaster);
+						//Shortcut is precedent shortcut
+							$query->leftJoin($parents[$key-1][1].'.'.$parent[0],$parent[1]);
+						}
+						//HERE WE ARE AFTER THE LEFT JOIN ON MASTER ACL CLASS WE PASS idsMaster array param
+						if(($key+1)==count($parents)){
+							//HERE FOR OBJECT CREATED BY CURRENT USER AND WITH STRICT MODE IS OF
+							if(count($ids) && method_exists($admin,'getMasterAclStrict') && !$admin->getMasterAclStrict()){
+								//OR EXPRESSION WITH PARENTHESIS
+								$orCondition = $query->expr()->orx();
+								$orCondition->add($query->expr()->in('o.id', ':ids'));
+								$orCondition->add($query->expr()->in($parent[1].'.id',':idsMaster'));
+								$query->andWhere($orCondition)->setParameter('ids', $ids)->setParameter('idsMaster', $idsMaster);
+							}else{
+								$query->andWhere($parent[1].'.id IN (:idsMaster'.$key.')')->setParameter('idsMaster'.$key, $idsMaster);
+							}
 						}
 					}
+				}else{
+					$query->andWhere('o.id IN (:idsMaster)')->setParameter('idsMaster', $idsMaster);
 				}
 				return;
             }elseif(count($ids)){
